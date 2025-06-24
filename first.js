972b91712d36b4b0abd468642d69d45993f21b8b
Saim <102881451+ahmed6913@users.noreply.github.com>
2025-06-24 23:46:36 +0530

    Update first.js

--- first.js
+++ first.js
@@ -2,14 +2,6 @@
 
 console.log("hello world");
 
-// comments 
-
-// single line comment
-
-/*
-    multi line comment
-*/
-
 // variables
 
    //let/ var/ const 
@@ -19,10 +11,6 @@ name = "ahmed";
 age = 20;
 
 console.log("hello " + name + " you are " + age + " years old");
-
-
-console.log(`hello ${name} you are ${age} years old`);
-
 
 
 // console and Console are same two diffrent var
