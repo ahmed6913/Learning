1e53133bb944fe220670310919470ad0b52718e8
Saim <102881451+ahmed6913@users.noreply.github.com>
2025-06-24 23:47:32 +0530

    Update MyToken.sol

--- MyToken.sol
+++ MyToken.sol
@@ -9,3 +9,22 @@ contract MyToken is ERC20 {
         _mint(msg.sender, initialSupply);
     }
 }
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract SimpleStorage {
+    // State variable to store a number
+    uint256 private storedNumber;
+
+    // Function to set the number
+    function set(uint256 _num) public {
+        storedNumber = _num;
+    }
+
+    // Function to get the number
+    function get() public view returns (uint256) {
+        return storedNumber;
+    }
+}
+c
