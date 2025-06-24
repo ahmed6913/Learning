8d47374ba925140123f7e61e138524f99429e5f5
Saim <102881451+ahmed6913@users.noreply.github.com>
2025-06-24 23:45:22 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,28 @@
+// syntax 
+
+console.log("hello world");
+
+// comments 
+
+// single line comment
+
+/*
+    multi line comment
+*/
+
+// variables
+
+   //let/ var/ const 
+
+name = "ahmed";
+
+age = 20;
+
+console.log("hello " + name + " you are " + age + " years old");
+
+
+console.log(`hello ${name} you are ${age} years old`);
+
+
+
+// console and Console are same two diffrent var
