397cbbcd2e0a8cfd71540fcb7c2732fbd92f2bb4
saim <ahmed6913@users.noreply.github.com>
2025-07-08 15:24:21 +0530

    go

--- learn.go
+++ learn.go
@@ -1,7 +1,12 @@
 package main
-import ("fmt")
 
-func main()  {
-	fmt.println("hello world")
-	
+import "fmt"
+
+func main() {
+	x := 5
+	y := 3
+
+	if x > y {
+		fmt.Println("x is greater than y")
+	}
 }
