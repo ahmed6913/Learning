e952e7415141dd2c1309088788f7a1555ec312a9
Saim <102881451+ahmed6913@users.noreply.github.com>
2025-07-28 19:13:47 +0530

    func

---
+++ functions/functions.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+
+func add(a , b int) int {
+	return a + b
+}
+
+
+func main()  {
+	fmt.Println(add(1, 2))
+
+
+
+}
+
+
+ 
---
+++ slices/slices.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+
+
+func main()  {
+	// slices are dynamic arrays
+
+	var fruits = []string{"apple", "banana", "cherry"}
+	fmt.Println(fruits)
+
+	
+	
+}
